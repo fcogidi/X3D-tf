e86a2066fa5c3a4c7375e566569ffffde7d8270c
Chianugo Ogidi <gmi672@usask.ca>
2020-11-01 19:06:50 -0600

    implemented bottleneck block

--- Bottleneck.py
+++ Bottleneck.py
@@ -1,48 +1,115 @@
-import tensorflow as tf
+from tensorflow.keras.layers import Layer
+from tensorflow.keras.layers import Conv3D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import AveragePooling3D
+from tensorflow.keras.layers import BatchNormalization
 
-class Bottleneck(tf.keras.layers.Layer):
-    def __init__(self, 
-                filters1: int, 
-                filters2: int, 
-                filters3: int) -> None:
+
+class Bottleneck(Layer):
+    '''
+    X3D Bottleneck layer: 1x1x1, Tx3x3, 1x1x1 with squeeze-excitation
+        added at every 2 stages.
+    '''
+    def __init__(self,
+                out_channels: int,
+                eps: float = 1e-5,
+                bn_mnt: float = 0.1,
+                block_index: int = 0,
+                se_ratio: float = 0.0625,
+                temp_kernel_size: int = 3) -> None:
+        '''Constructs a single X3D bottlenet block.
+
+        Args:
+            out_channels (int): number of output channels
+            eps (float): epsilon for batch norm (default: 1e-5)
+            bn_mnt (float): momentum for batch norm (default: 0.1)
+            block_index (int): the index of the current block
+            se_ratio (float): the width multiplier for the squeeze-excitation
+                operation (default: 0.0625)
+            temp_kernel_size (int): number of filters to use in the temporal 
+                dimension for 3x3x3 conv (default: 3)
+        '''
         super(Bottleneck, self).__init__()
-        self.block1 = tf.keras.layers.Conv3D(filters=filters1, 
-                                            kernel_size=1,
-                                            strides=(1, 1, 1),
-                                            padding='same',
-                                            use_bias=False,
-                                            data_format='channels_last')
-        self.batch_norm1 = tf.keras.layers.BatchNormalization(axis=-1)
-        self.relu1 = tf.keras.layers.Activation('relu')
-        self.block2 = tf.keras.layers.Conv3D(filters=filters2, 
-                                            kernel_size=3,
-                                            strides=(1, 1, 1),
-                                            padding='same',
-                                            use_bias=False,
-                                            data_format='channels_last')
-        self.batch_norm2 = tf.keras.layers.BatchNormalization(axis=-1)
-        self.relu2 = tf.keras.layers.Activation('relu')
-        self.block3 = tf.keras.layers.Conv3D(filters=filters3, 
-                                            kernel_size=1,
-                                            strides=(1, 1, 1),
-                                            padding='same',
-                                            use_bias=False,
-                                            data_format='channels_last')
-        self.batch_norm3 = tf.keras.layers.BatchNormalization(axis=-1)
-        self.add_op = tf.keras.layers.Add()
-        self.swish = tf.keras.layers.Activation('swish')
+        self.block_index = block_index
+        self.a = Conv3D(filters=out_channels, 
+                        kernel_size=1,
+                        strides=(1, 1, 1),
+                        padding='same',
+                        use_bias=False,
+                        data_format='channels_last')
+        self.bn_a = BatchNormalization(axis=-1,
+                                        momentum=bn_mnt, 
+                                        epsilon=eps)
+        self.relu1 = Activation('relu')
+        self.b = Conv3D(filters=out_channels, 
+                        kernel_size=(temp_kernel_size, 3, 3),
+                        strides=(1, 1, 1),
+                        padding='same',
+                        use_bias=False,
+                        data_format='channels_last')
+        self.bn_b = BatchNormalization(axis=-1,
+                                        momentum=bn_mnt, 
+                                        epsilon=eps)
+        self.relu2 = Activation('relu')
+
+        # Squeeze-and-Excite operation
+        if (self.block_index % 2 == 0):
+            width = self._round_width(out_channels, se_ratio)
+            self.se_pool = AveragePooling3D(pool_size=(1, 1, 1))
+            self.se_fc1 = Conv3D(filters=width,
+                                kernel_size=1,
+                                strides=1, 
+                                use_bias=True, 
+                                activation='relu')
+            self.se_fc2 = Conv3D(filters=out_channels,
+                                kernel_size=1, 
+                                strides=1, 
+                                use_bias=True, 
+                                activation='sigmoid')
+
+        self.c = Conv3D(filters=out_channels, 
+                        kernel_size=1,
+                        strides=(1, 1, 1),
+                        padding='same',
+                        use_bias=False,
+                        data_format='channels_last')
+        self.bn_c = BatchNormalization(axis=-1)
 
     def call(self, input, training=False):
-        layer = self.block1(input)
-        layer = self.batch_norm1(layer)
-        layer = self.relu1(layer)
-        layer = self.block2(layer)
-        layer = self.batch_norm2(layer)
-        layer = self.relu2(layer)
-        layer = self.block3(layer)
-        layer = self.batch_norm3(layer)
-        
-        output = self.add_op([input, layer])
-        output = self.swish(output)
+        out = self.a(input)
+        out = self.bn_a(out)
+        out = self.relu1(out)
+        out = self.b(out)
+        out = self.bn_b(out)
+        out = self.relu2(out)
+        if (self.block_index % 2 == 0):
+            se = self.se_pool(out)
+            se = self.se_fc1(se)
+            se = self.se_fc2(se)
+            out = out * se
+        out = self.c(out)
+        out = self.bn_c(out)
 
-        return output
+        return out
+
+    def _round_width(self, width, multiplier, min_width=8, divisor=8):
+        """
+        Round width of filters based on width multiplier
+        Args:
+            width (int): the channel dimensions of the input.
+            multiplier (float): the multiplication factor.
+            min_width (int): the minimum width after multiplication.
+            divisor (int): the new width should be dividable by divisor.
+        from: https://github.com/facebookresearch/SlowFast/blob/master/slowfast/models/operators.py
+        """
+        if not multiplier:
+            return width
+
+        width *= multiplier
+        min_width = min_width or divisor
+        width_out = max(
+            min_width, int(width + divisor / 2) // divisor * divisor
+        )
+        if width_out < 0.9 * width:
+            width_out += divisor
+        return int(width_out)
--- test_bottleneck.py
+++ test_bottleneck.py
@@ -2,13 +2,54 @@ from Bottleneck import Bottleneck
 import tensorflow as tf
 import time
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+
+if gpus:
+  for gpu in gpus:
+    try:
+      tf.config.experimental.set_memory_growth(gpu, True)
+      logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+      print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+      print(e)
+
+tf.random.set_seed(42069)
+
 if __name__ == "__main__":
-    input = tf.ones(shape=(1, 16, 224, 224, 3))
-    input = tf.keras.activations.swish(input)
-    res_block = Bottleneck(24, 24, 3)
+    input = tf.random.uniform(shape=(1, 8, 112, 112, 3), maxval=256)
+    print("Input shape: ", input.shape)
+
+    spatial_paddings = tf.constant([[0, 0], [0, 0], [1, 1], [1, 1], [0, 0]])
+    temp_paddings = tf.constant([[0, 0], [2, 2], [0, 0], [0, 0], [0, 0]])
+
+    spatial_input = tf.pad(input, spatial_paddings)
+    
+    spatial_conv = tf.keras.layers.Conv3D(filters=24, 
+                            kernel_size=(1, 3, 3),
+                            strides=(1, 2, 2),
+                            use_bias=False,
+                            data_format='channels_last')(spatial_input)
+    print("Spatial conv shape: ", spatial_conv.shape)
+
+    temp_input = tf.pad(spatial_conv, temp_paddings)
+
+    temp_conv = tf.keras.layers.Conv3D(filters=24, 
+                            kernel_size=(5, 1, 1),
+                            strides=(1, 1, 1),
+                            use_bias=False,
+                            data_format='channels_last')(temp_input)
+    print("Temp. conv shape: ", temp_conv.shape)
+    conv1 = tf.keras.layers.BatchNormalization(axis=-1, momentum=0.1, epsilon=1e-5)(temp_conv)
+
+    res_block = Bottleneck(out_channels=24)
 
     start_time = time.time()
-    output = res_block(input)
+    output = res_block(conv1)
+
+    print("Output shape: ", output.shape)
+
+    add_op = tf.keras.layers.Add()
+    swish = tf.keras.layers.Activation('swish')
     print("Runtime: %.4fs" %(time.time() - start_time))
     #tf.print('Input: ', input)
     #tf.print('Output: ', output)
